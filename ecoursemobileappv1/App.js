cef006469c0b214029555e79b3cc6c4971d46b92
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2025-04-02 11:32:24 +0700

    create mobile app

--- ecoursemobileappv1/App.js
+++ ecoursemobileappv1/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./components/Home/Home";
+import Login from "./components/User/Login";
+import { Icon } from "react-native-paper";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Lesson from "./components/Home/Lesson";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen name="home" component={Home}  />
+      <Tab.Screen name="lesson" component={Lesson}  />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="index" component={StackNavigator} options={{tabBarIcon: () => <Icon source="home" size={20} />}}  />
+      <Tab.Screen name="login" component={Login} options={{tabBarIcon: () => <Icon source="account" size={20} />}} />
+    </Tab.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ ecoursemobileappv1/components/User/Login.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import MyStyles from "../../styles/MyStyles";
+
+const Login = () => {
+    return (
+        <Text style={MyStyles.subject}>LOGIN</Text>
+    );
+}
+
+export default Login;
